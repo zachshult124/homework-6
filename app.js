889ddea9c0ebbbb6d4a67894854aa3b313904dfc
zachshult <zachshult12345@gmail.com>
2018-08-15 23:38:56 -0500

    more
    Additional functionality

---
+++ app.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+
+    var gifCount = 0;
+
+    $("add-gif-button").on('click', function (event) {
+        event.preventDefault();
+
+        //gets the gif that we need to search and returns it to the gifToReturn Variable
+        var gifToReturn = $("#searchThisGif").val().trim();
+
+        //creates a p that will hold all the gif's we create
+        var createParagraph = $("<p>");
+        //adds an 
+        createParagraph.attr("id", item)
+    });
+
+
+})
